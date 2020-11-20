ddff50d86366e7a69e463d3c6b6a0384680e635c
andreeabizu <andreeabizu@yahoo.com>
2020-11-20 10:30:50 +0200

    Analiza

--- src/WebServer/Main.java
+++ src/WebServer/Main.java
@@ -4,46 +4,16 @@ public class Main {
     public static void main(String[] arg) throws InterruptedException {
 
 
-          WebServer server = new WebServer();
-          Thread initial = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    server.setPort(8080);
+        WebServer server = new WebServer();
 
-                }
-            });
-            initial.start();
+        server.setPort(8080);
+        Thread.sleep(3000);
 
-            Thread.sleep(4000);
-            System.out.println("acum");
-
-        Thread.sleep(1000);
-           server.wakeUp();
-        Thread.sleep(4000);
+        server.wakeUp();
+        Thread.sleep(3000);
         server.maintenance();
-        Thread.sleep(4000);
+        Thread.sleep(3000);
         server.stop();
-        Thread.sleep(4000);
-        Thread initial1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                server.setPort(8081);
-
-            }
-        });
-        initial1.start();
-       Thread.sleep(4000);
-        System.out.println("acum");
-
-        Thread.sleep(1000);
-        Thread initial2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                server.setPort(8080);
-
-            }
-        });
-        initial2.start();
 
     }
 }
